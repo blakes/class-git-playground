1b39147801d7a92901517a0af7dccb5ac87e6838
blakes <blake.j.symington@gmail.com>
2019-08-30 11:27:54 +1000

    Initial Commit

---
+++ index.rb
@@ -0,0 +1,14 @@
+require 'colorize'
+
+puts "This is light blue".colorize(:light_blue)
+sleep(0.5)
+puts "This is blue".colorize(:blue)
+sleep(0.5)
+puts "This is green".colorize(:green)
+sleep(0.5)
+puts "This is yellow".colorize(:yellow)
+sleep(0.5)
+puts "This is red".colorize(:red)
+sleep(0.5)
+puts "This is pink".colorize(:pink)
+sleep(0.5)
